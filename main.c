328640cd5ba04c42a69262518dad85e07a5c697b
enfycius <hexk0131@gmail.com>
2022-01-19 22:08:32 +0900

    fix #1

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
-include <stdio.h>
+int main(void) {
+    int a, b;
 
-int main() {
-	int a, b, sum;
-	printf("Input two integers to add: ");
-	scanf("%d %d", &a, &b");
-	sum = a + b;
-	printf("%d", sum);
-	return 0;
+    printf("%d + %d = %d\n", a, b, a+b);
+
+    //
+
+    return 0;
 }
